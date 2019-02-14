403928de47dbac3949eb9b31950490cbecd0f5ab
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-02-14 17:56:14 +0100

    stockfish.wasm: Consistent formatting for pre.js

--- src/pre.js
+++ src/pre.js
@@ -1,46 +1,46 @@
 (function () {
-    // Message listeners
+  // Message listeners
 
-    var listeners = [];
+  var listeners = [];
 
-    Module.print = function(line) {
-      if (listeners.length === 0) console.log(line);
-      for (var i = 0; i < listeners.length; i++) listeners[i](line);
-    };
+  Module.print = function(line) {
+    if (listeners.length === 0) console.log(line);
+    for (var i = 0; i < listeners.length; i++) listeners[i](line);
+  };
 
-    Module.addMessageListener = function(listener) {
-      listeners.push(listener);
-    };
+  Module.addMessageListener = function(listener) {
+    listeners.push(listener);
+  };
 
-    Module.removeMessageListener = function(listener) {
-      var idx = listeners.indexOf(listener);
-      if (idx >= 0) listeners.splice(idx, 1);
-    };
+  Module.removeMessageListener = function(listener) {
+    var idx = listeners.indexOf(listener);
+    if (idx >= 0) listeners.splice(idx, 1);
+  };
 
-    // Command queue
+  // Command queue
 
-    var queue = [], backoff = 1;
+  var queue = [], backoff = 1;
 
-    function poll() {
-      var command = queue.shift();
-      if (!command) return;
+  function poll() {
+    var command = queue.shift();
+    if (!command) return;
 
-      var tryLater = Module.ccall('uci_command', 'number', ['string'], [command]);
-      if (tryLater) queue.unshift(command);
-      backoff = tryLater ? (backoff * 2) : 1;
-      setTimeout(poll, backoff);
-    }
+    var tryLater = Module.ccall('uci_command', 'number', ['string'], [command]);
+    if (tryLater) queue.unshift(command);
+    backoff = tryLater ? (backoff * 2) : 1;
+    setTimeout(poll, backoff);
+  }
 
+  Module.postMessage = function(command) {
+    queue.push(command);
+  };
+
+  Module.postRun = function() {
     Module.postMessage = function(command) {
       queue.push(command);
+      if (queue.length == 1) poll();
     };
 
-    Module.postRun = function() {
-      Module.postMessage = function(command) {
-        queue.push(command);
-        if (queue.length == 1) poll();
-      };
-
-      poll();
-    };
+    poll();
+  };
 })();
