9a3ab34f05b45de96408ebc064d5efdd802dcf23
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-08-04 11:17:14 +0200

    stockfish.wasm: Modernize pre.js

--- src/pre.js
+++ src/pre.js
@@ -3,18 +3,18 @@
 
   const listeners = [];
 
-  Module['print'] = function(line) {
+  Module['print'] = line => {
     if (listeners.length === 0) console.log(line);
-    else setTimeout(function() {
+    else setTimeout(() => {
       for (const listener of listeners) listener(line);
-    }, 1);
+    });
   };
 
-  Module['addMessageListener'] = function(listener) {
+  Module['addMessageListener'] = listener => {
     listeners.push(listener);
   };
 
-  Module['removeMessageListener'] = function(listener) {
+  Module['removeMessageListener'] = listener => {
     const idx = listeners.indexOf(listener);
     if (idx >= 0) listeners.splice(idx, 1);
   };
@@ -24,26 +24,25 @@
   const queue = [];
   let backoff = 1;
 
-  function poll() {
+  const poll = () => {
     const command = queue.shift();
-    if (!command) return;
+    if (command === undefined) return;
 
     const tryLater = Module.ccall('uci_command', 'number', ['string'], [command]);
     if (tryLater) queue.unshift(command);
     backoff = tryLater ? (backoff * 2) : 1;
     setTimeout(poll, backoff);
-  }
+  };
 
-  Module['postMessage'] = function(command) {
+  Module['postMessage'] = command => {
     queue.push(command);
   };
 
-  Module['postRun'] = function() {
-    Module['postMessage'] = function(command) {
+  Module['postRun'] = () => {
+    Module['postMessage'] = command => {
       queue.push(command);
-      if (queue.length == 1) poll();
+      if (queue.length === 1) poll();
     };
-
     poll();
   };
 })();
